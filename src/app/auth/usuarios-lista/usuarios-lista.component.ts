a1d7f30baf4fe3f82343389bd819392ae1d046ee
jonhrabit <jonhrabit@gmail.com>
2025-05-24 23:20:16 -0300

    primeiro commit

---
+++ src/app/auth/usuarios-lista/usuarios-lista.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios-lista',
+  imports: [],
+  templateUrl: './usuarios-lista.component.html',
+  styleUrl: './usuarios-lista.component.scss'
+})
+export class UsuariosListaComponent {
+
+}
